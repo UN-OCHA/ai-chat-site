b37fafe2d998d225dc57a14aa26361203be3ce35
orakili <github@orakili.net>
2023-10-19 00:49:33 +0000

    chore: better mock servers for azure openai and aws bedrock

--- local/embedding/app.py
+++ local/embedding/app.py
@@ -9,24 +9,43 @@ model_folder = os.environ.get('MODEL_FOLDER', '/opt/models')
 model = SentenceTransformer(model_name_or_path=model_name, cache_folder=model_folder)
 model.max_seq_length = 256
 
-class Embedding(BaseModel):
+app = FastAPI()
+
+class BedrockRequest(BaseModel):
+    inputText: str
+
+class BedrockResponse(BaseModel):
+    embedding: List[float]
+
+# Mimic the Bedrock embeddings endpoint.
+@app.post("/bedrock/model/{model}/invoke")
+def embed(request: BedrockRequest) -> BedrockResponse:
+    embedding = model.encode(request.inputText)
+    return BedrockResponse(embedding=embedding)
+
+class OpenAiEmbedding(BaseModel):
     embedding: List[float]
     index: int
     object: str
 
-class Request(BaseModel):
+class OpenAiUsage(BaseModel):
+    prompt_tokens: int
+    total_tokens: int
+
+class OpenAiRequest(BaseModel):
     input: List[str]
     model: str
 
-class Response(BaseModel):
-    data: List[Embedding]
+class OpenAiResponse(BaseModel):
+    data: List[OpenAiEmbedding]
     object: str
+    model: str
+    usage: OpenAiUsage
 
-app = FastAPI()
-
-# Mimic the OpenAI embeddings endpoint with the minimum required fields.
-@app.post("/v1/embeddings")
-def embed(request: Request):
+# Mimic the Azure OpenAI embeddings endpoint.
+@app.post("/openai/deployments/{deployment}/embeddings")
+def embed(request: OpenAiRequest) -> OpenAiResponse:
     embeddings = model.encode(request.input)
-    data = [Embedding(embedding=embedding, index=index, object="embedding") for index, embedding in enumerate(embeddings)]
-    return Response(data=data, model=request.model, object="list")
+    usage=OpenAiUsage(prompt_tokens=1,total_tokens=1)
+    data = [OpenAiEmbedding(embedding=embedding, index=index, object="embedding") for index, embedding in enumerate(embeddings)]
+    return OpenAiResponse(data=data, object="list", model=request.model, usage=usage)
